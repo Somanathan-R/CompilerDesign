8b1bdac84dcaf58d6ac86c6deace5526e6c917d5
Somanathan R <103750948+Somanathan-R@users.noreply.github.com>
2024-03-19 16:30:58 +0530

    Add files via upload

---
+++ week7/Jpgm3.java
@@ -0,0 +1,24 @@
+import java.io.*;  
+import java.util.*;  
+public class Jpgm3  
+{  
+ public static void main(String[] args)  
+ {  
+ //Declare the necessary variables  
+ int a,b,c;  
+  
+ //Create Scanner Object to get input from keyboard   
+ Scanner sc = new Scanner(System.in);    
+ //Get the 1st Number  
+ System.out.println("Enter first Number");   
+ a = sc.nextInt();  
+  
+ //Get the 2nd Number  
+ System.out.println("Enter second Number");   
+ b = sc.nextInt();  
+ //Find the Addition  
+ c = a +b ;  
+ //Print the result  
+ System.out.println("The Addition is:"  + c);   }  
+}
+
